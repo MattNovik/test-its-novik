5d92d3903bce6dab1b373df1664fc144febfdba5
mattNov <matvej.novik@gmail.com>
2022-08-29 17:00:52 +0300

    some style + swipe

--- source/js/modules/init-swipes.js
+++ source/js/modules/init-swipes.js
@@ -9,19 +9,19 @@ const sortModal = document.querySelector(".modal--sort .modal__content");
 const initSwipe = () => {
   let initialPoint;
   let finalPoint;
-  document.addEventListener(
+  filterModal.addEventListener(
     "touchstart",
     function (event) {
-      event.preventDefault();
-      event.stopPropagation();
+      //event.preventDefault();
+      //event.stopPropagation();
       initialPoint = event.changedTouches[0];
     },
     false
   );
-  document.addEventListener(
+  filterModal.addEventListener(
     "touchend",
     function (event) {
-      event.preventDefault();
+      //event.preventDefault();
       event.stopPropagation();
       finalPoint = event.changedTouches[0];
       let xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
@@ -32,13 +32,41 @@ const initSwipe = () => {
           if (finalPoint.pageY < initialPoint.pageY) {
             /*СВАЙП ВВЕРХ*/
           } else {
-            if (filterModal && sortModal) {
+            if (filterModal) {
               modals.close("modal-mob-filter");
+            }
+          }
+        }
+      }
+    },
+    false
+  );
+
+  sortModal.addEventListener(
+    "touchstart",
+    function (event) {
+      //event.preventDefault();
+      //event.stopPropagation();
+      initialPoint = event.changedTouches[0];
+    },
+    false
+  );
+  sortModal.addEventListener(
+    "touchend",
+    function (event) {
+      //event.preventDefault();
+      event.stopPropagation();
+      finalPoint = event.changedTouches[0];
+      let xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+      let yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+      if (xAbs > 20 || yAbs > 20) {
+        if (xAbs > yAbs) {
+        } else {
+          if (finalPoint.pageY < initialPoint.pageY) {
+            /*СВАЙП ВВЕРХ*/
+          } else {
+            if (sortModal) {
               modals.close("modal-sort");
-            } else if (sortModal) {
-              modals.close("modal-sort");
-            } else if (filterModal) {
-              modals.close("modal-mob-filter");
             }
           }
         }
